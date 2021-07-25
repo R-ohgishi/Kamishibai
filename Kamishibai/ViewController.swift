2f8fd9349fb265f643e7f373dfb06816fa1939ff
Syu H <shpab@icloud.com>
2021-07-25 11:27:58 +0900

    1

--- Kamishibai/ViewController.swift
+++ Kamishibai/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // hashimoto 1125
     }
 //飛田変更
     
