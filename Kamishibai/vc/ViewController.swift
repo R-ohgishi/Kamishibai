4fa628da048bcc895745d28b77112b415dd73415
R-Ohgishi <ryuji.ohgishi.app@gmail.com>
2021-08-01 11:10:29 +0900

    3日目の授業内容を反映

---
+++ Kamishibai/vc/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Kamishibai
+//
+//  Created by user on 2021/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //画面が初期化されるときに実行される関数
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // hashimoto 1125
+    }
+//飛田変更
+    @IBAction func buttonTouchDown(_ sender: Any) {
+    
+    }
+    
+    @IBAction func Btn(_ sender: Any) {
+    }
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "nextViewSegue", sender: sender)
+    }
+    
+    
+    //画面のデータ受け渡し方法(今回は#3を使っています)
+    //https://qiita.com/fromage-blanc/items/3ea2dfe97d4c2d6f0646
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
+    func AAA(){
+        
+    }
+    
+}
+
