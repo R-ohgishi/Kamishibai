aa7f79905a1ce0657aa1818393641487738828e7
shpab <75552289+shpab@users.noreply.github.com>
2021-08-08 21:56:35 +0900

    1

---
+++ Kamishibai/vc/MomoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MomoViewController.swift
+//  Kamishibai
+//
+//  Created by 橋本周三 on 8/8/21.
+//
+
+import UIKit
+
+class MomoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
