7fac987d7a040175ff4210298dbcccdba483c316
shpab <75552289+shpab@users.noreply.github.com>
2021-08-09 11:45:22 +0900

    1

--- Kamishibai/vc/NextViewController.swift
+++ Kamishibai/vc/NextViewController.swift
@@ -8,36 +8,40 @@
 import UIKit
 
 class NextViewController: UIViewController {
-
-
+    
+    
+    
     
     @IBOutlet weak var momotaroViewSegue: UIImageView!
     
-        
-        
-        
-        override func  viewDidLoad() {
-            super.viewDidLoad()
-        }
-
     
-    MomoViewController.isUserInteractionEnable = true
-    MomoViewController.adGestureRecognizer(JITapGestureRecongnizer(taget: self, action: #selecter(tapped))
+    
+    override func  viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        momotaroViewSegue.isUserInteractionEnabled = true
+        momotaroViewSegue.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapped)))
         // Do any additional setup after loading the view.
     }
     
     @objc func tapped(){
-            performSegue(withIdentifier: "momotaroViewSegue", sender: self)
+        performSegue(withIdentifier: "momotaroViewSegue1", sender: self)
     }
-
+    
+    
+    
+    
+    // Do any additional setup after loading the view.
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
