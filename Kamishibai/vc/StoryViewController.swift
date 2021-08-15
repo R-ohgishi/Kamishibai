31188e4aad0159d4e923aea4f4487748dfb60609
RyujiOhgishi1101 <ryuuji.ohgishi@jp.ricoh.com>
2021-08-15 10:15:46 +0900

    fix

--- Kamishibai/vc/StoryViewController.swift
+++ Kamishibai/vc/StoryViewController.swift
@@ -11,6 +11,7 @@ class StoryViewController: UIViewController {
 
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var story: UILabel!
+    var page:Int = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +21,13 @@ class StoryViewController: UIViewController {
     
     @IBAction func nextStoryBtn(_ sender: Any) {
         let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "story") as! StoryViewController
+        
+        secondViewController.loadView()
+        secondViewController.image.image = UIImage(named: "jijibaba")
+        
+        secondViewController.page = page + 1
+        secondViewController.story.text = "画面遷移しました\(secondViewController.page)"
+        
         self.show(secondViewController, sender: self)
     }
     
